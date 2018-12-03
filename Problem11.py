ab346913aca01bdb478946827d97963647ffcbd9
HaroldChang <hoch7198@colorado.edu>
2018-12-02 21:40:23 -0700

    workMonday

--- Problem11.py
+++ Problem11.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 import pandas as pd
 import scipy
@@ -6,7 +5,7 @@ import scipy
 import statsmodels
 import statsmodels.api as sm
 from statsmodels.formula.api import ols
-#Woked with Lucas, Andrew
+#Woked with Lucas, Andrew, Anastasia, Steve
 
 #1. What statistical test would you use for the following scenarios? 
 
@@ -49,6 +48,12 @@ def runAnova(data, formula):
     aov_table= sm.stats.anova_lm(model, typ=2)
     print(aov_table)
     
+def percentage(df):
+    df['Total'] = df['Admitted'] + df['Rejected']
+    df['Acceptance_R'] = df['Admitted']/df['Total']
+    df['Rejection_R'] = df['Rejected']/df['Total']
+    return df
+    
     
 rawData, df = generateDataset('simpsons_paradox.csv')
 
@@ -62,14 +67,28 @@ print("Does department correlate with admissions?")
 simpleFormula = 'Admitted ~ C(Department)'
 runAnova(rawData, simpleFormula)
 
+print("Do gender AND department correlate with admissions?")
+moreComplex = 'Admitted ~ C(Department) + C(Gender)'
+runAnova(rawData, moreComplex)
 
-#Ttest_indResult(statistic=5.332277756733584, pvalue=0.001774285663548817)
 
- #P value is less than 0.05, so I will reject the null hypothesis
+#Does gender correlate with admissions?
+#Ttest_indResult(statistic=5.332277756733584, #pvalue=0.001774285663548817)
+#Does department correlate with admissions?
+                  #sum_sq   df         F    PR(>F)
+#C(Department)   92266.75  5.0  0.737438  0.622205
+#Residual       150141.50  6.0       NaN       NaN
+#Do gender AND department correlate with admissions?
+                      #sum_sq   df          F    PR(>F)
+#C(Department)   41153.333333  5.0   7.515304  0.036670
+#C(Gender)      145760.750000  2.0  66.546025  0.000851
 
-#I would say the analysis is very bias, since the training data is very accurate
+# Based on the p_value (lower than 0.05), I'd conclude that the reuslt was not very bias. Additionally, I think "gender"  appear to contribute most heavily to admissions.
 
- #Also I don't think they are very correlated
+#Problem 3:
+#Answer: The data quality issue was that the test was analysing "Admit", and "reject" separately. We will have to combine the total number of "admit" and "reject". then use that combined rate to check the acceptance rate. Total/adimitted. They are similar in the results of "Does department correlate with admission". On the other hand the p-value was different.
+
+
 
 
 
